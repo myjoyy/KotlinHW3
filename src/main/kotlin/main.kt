384b56f546f7e0ac4a890ace0175dd11562946b0
Joy Test <blossk@outlook.com>
2022-12-17 12:49:50 +0300

    Modify fun

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,13 @@
-const val MASTERCARD = "MasterCard"
-const val VISA = "VISA"
-const val VKPAY ="VKPay"
+import kotlin.math.max
+
 const val ERR_LIMIT = -1
 const val ERR_TYPE = -2
 fun main () {
     println("TASK #1")
-    println("Был(а) в сети ${agoToText()}")
+    println("Был(а) в сети ${agoToText(seconds = 1000)}") // измените seconds для результата
 
     println("TASK #2")
-    val result = comission(VISA, 81440, 30)
+    val result = comission("Visa", 10000, 10000) // TYPE: MasterCard, Maestro, Visa, Mir, VKPay
     when (result) {
         ERR_TYPE -> println("Wrong Type")
         ERR_LIMIT -> println("Limit exceed")
@@ -17,28 +16,33 @@ fun main () {
 
 }
 
-fun comission(type: String, amount: Int, previous: Int) : Any =
-    when (type) {
-        MASTERCARD -> if (amount + previous > 150000) ERR_LIMIT else if (amount <= 75000 && previous <= 600000) 0 else (amount/100) * 0.6 + 20
-        VISA -> {
+fun comission(type: String, amount: Int, previous: Int): Int {
+    return when (type) {
+        "MasterCard", "Maestro" -> return if (amount + previous > 150000) -1 else max(35, (amount * 0.005).toInt())
+
+        "Visa", "Mir" -> {
             when {
                 amount + previous <= 10000 -> 0
                 amount + previous > 600000 -> ERR_LIMIT
-                amount <= 35 -> ERR_LIMIT
-                else -> amount/100 * 0.75
+                else -> max(35, (amount * 0.005).toInt())
             }
         }
-        VKPAY -> {
+
+        "VKPay" -> {
             when {
-                amount >= 15000 -> ERR_LIMIT
+                amount > 15000 -> ERR_LIMIT
                 amount + previous >= 40000 -> ERR_LIMIT
                 else -> 0
             }
         }
-        else -> ERR_TYPE
+
+
+        else -> {
+            ERR_TYPE
+        }
     }
-fun agoToText(): Any {
-    val seconds = 17000 // TIME AGO IN SECONDS
+}
+fun agoToText(seconds: Int): String {
     val minutes = if (seconds/60 % 1 == 1 && seconds/60 <= 241 ) "минут" else if (seconds/60 % 2 == 0) "минут" else "минуты"
     val hours = if (seconds/3600 % 1 == 1 && seconds/3600 <= 3601) "час" else if (seconds/3600 % 2 == 0) "часа" else "часов"
     return when (seconds) {
